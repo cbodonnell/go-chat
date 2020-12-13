5b0105d04a6919c3251f69932baf865948e5f7b8
cbodonnell <cheebzmusic@gmail.com>
2020-12-13 00:56:38 -0500

    middleware logging

--- go-chat.go
+++ go-chat.go
@@ -150,11 +150,13 @@ func handleMessages() {
 
 func jwtMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := checkClaims(r)
+		claims, err := checkClaims(r)
 		if err != nil {
 			unauthorizedRequest(w, err)
 			return
 		}
+		fmt.Println(r.RequestURI)
+		fmt.Println(claims)
 		h.ServeHTTP(w, r)
 	})
 }

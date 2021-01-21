3fdf763d5e57270e68b3b592f76e2f33cae8808c
cbodonnell <cheebzmusic@gmail.com>
2021-01-20 22:38:01 -0500

    logging

--- go-chat.go
+++ go-chat.go
@@ -134,9 +134,11 @@ func jwtMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		claims, err := checkClaims(r)
 		if err != nil {
+			fmt.Println("Unauthorized request from " + r.RemoteAddr + " - " + err.Error())
 			unauthorizedRequest(w, err)
 			return
 		}
+		fmt.Println(claims)
 		h.ServeHTTP(w, r)
 	})
 }
